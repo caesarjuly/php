c1c9deb96c43b8e349ad330edc8f1976292899dc
caesarjuly <251138775@qq.com>
2013-04-24 21:02:25 +0800

    new graph sample

---
+++ graphSample/simplegraph.php
@@ -0,0 +1,21 @@
+<?php
+	//set up image
+	$height = 200;
+	$width = 200;
+	$im = imagecreatetruecolor($width, $height);
+	$white = imagecolorallocate($im, 255, 255, 255);
+	$blue = imagecolorallocate($im, 0, 0, 64);
+
+	//draw on image 
+	imagefill($im, 0, 0, $blue);
+	imageline($im, 0, 0, $width, $height, $white);
+	imagestring($im, 4, 50, 150, 'Sales', $white);
+
+	//output image
+	header('Content-type: image/png');
+	imagepng($im);
+
+	//clean up
+	imagedestroy($im);
+
+?>
